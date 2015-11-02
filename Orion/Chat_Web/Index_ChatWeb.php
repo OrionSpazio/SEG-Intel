f50e84a95e87f2b51d98b57334e18250e255d203
OrionSpazio <gdrol038@uottawa.ca>
2015-11-02 10:52:04 -0500

    voila

--- Orion/Chat_Web/Index_ChatWeb.php
+++ Orion/Chat_Web/Index_ChatWeb.php
@@ -64,7 +64,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["idcommunications"]. "<br>";
+        echo $row["idcommunications"]. "&#13;&#10;";
     }
 } else {
     echo "0 results";
