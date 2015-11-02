b33dd9125e54f1bc70eff23479b94ea0f33b9398
OrionSpazio <gdrol038@uottawa.ca>
2015-11-01 21:33:16 -0500

    ;;;;;;;;;;;;; never forget these ->;;;;;;;

--- Orion/Chat_Web/Chat.php
+++ Orion/Chat_Web/Chat.php
@@ -7,7 +7,7 @@ $username = "baf69364474490";
 $password = "e8c2966f";
 $dbname = "seg-intel";
 $array = array("Gabriel","<br>",$_POST["text"],"<br>","<br>");
-$text = implode("<br>",$array)
+$text = implode("<br>",$array);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
